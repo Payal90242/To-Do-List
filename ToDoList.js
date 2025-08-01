aef918c65f58731e9062b60f1808c30e85ee8070
Payal Jajoriya <payaljajoriya5@gmail.com>
2025-08-02 00:28:22 +0530

    Add To-Do-List

---
+++ ToDoList.js
@@ -0,0 +1,33 @@
+const inputBox=document.getElementById("input-box");
+const listContainer= document.getElementById("list-container");
+function addTask(){
+if(inputBox.value===''){
+alert("You must write something!");
+} else{
+let li=document.createElement("li");
+li.innerHTML=inputBox.value;
+listContainer.appendChild(li);
+let span=document.createElement("span");
+span.innerHTML="\u00d7";
+li.appendChild(span);;
+}
+inputBox.value="";
+saveData();
+}
+listContainer.addEventListener("click",function(e){
+if(e.target.tegName==="LI"){
+e.target.classList.toggle("checked");
+saveData();
+}
+ else if(e.target.tegName==="SPAN"){
+e.target.parentElement.remove();
+saveData();
+}
+},flase);
+function saveData(){
+localStorage.setItem("data", listContainer.innerHTML);
+}
+function showList(){
+listContainer.innerHTML=localStorage.getItem("data");
+}
+showList();
